42b0c7d99c01e8e3199645f9eb91027a45f5176c
Mertcan.Elitas <mertcan.elitas@bilgeadam.com>
2021-06-24 22:02:50 +0300

    Redis Service Implementation Development.

---
+++ Services/Basket/Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Basket.Dtos
+{
+    public class BasketDto
+    {
+        public BasketDto()
+        {
+            BasketItems = new List<BasketItemDto>();
+        }
+
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                var total = BasketItems.Sum(x => x.Price * x.Quatity);
+
+                return total;
+            }
+        }
+
+        public List<BasketItemDto> BasketItems { get; set; }
+    }
+}
---
+++ Services/Basket/Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quatity { get; set; }
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
